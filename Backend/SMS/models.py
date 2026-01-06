8080868fb3d2adfe167b495032e64f2a345e7e4b
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2026-01-06 12:23:16 +0500

    Create Custom User Model add more field to AbstractUser in Django Define model

--- Backend/SMS/models.py
+++ Backend/SMS/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Django already provides:
+    # id (BigAutoField)
+    # username
+    # password (hashed)
+    # last_login
+    # is_active
+    # is_staff
+    # is_superuser
+    # date_joined
+
+    email = models.EmailField(unique=True,db_index=True,verbose_name="email address")
+    email_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20,unique=True,null=True,blank=True)
+    address = models.CharField(max_length=255,null=True,blank=True)
+    profile_image = models.ImageField(upload_to="/profileImages/users",null=True,blank=True)
+
+    class Meta:
+        db_table = "app_users"
+        ordering = ["-date_joined"]
+
+    def __str__(self):
+        return self.email
+
