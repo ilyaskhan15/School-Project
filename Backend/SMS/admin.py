32e7cd564e411c1d8d1a34d2f72de4b1c7d97c01
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2026-01-06 16:25:31 +0500

    Create SuperUser, Add register userModel in the adminpannel, and change the style of code little bit in model.py

--- Backend/SMS/admin.py
+++ Backend/SMS/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
+class UserProfileAdminModel(admin.ModelAdmin):
+    list_display = ('profile_image', 'full_name', 'email', 'is_superuser', 'email_verified')
+
+
+admin.site.register(models.User, UserProfileAdminModel)
 # Register your models here.
--- Backend/SMS/models.py
+++ Backend/SMS/models.py
@@ -3,6 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True,db_index=True,verbose_name="email address")
+    email_verified = models.BooleanField(default=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20,unique=True,null=True,blank=True)
+    address = models.CharField(max_length=255,null=True,blank=True)
+    profile_image = models.ImageField(upload_to="profileImages/users",null=True,blank=True)
     # Django already provides:
     # id (BigAutoField)
     # username
@@ -13,19 +21,13 @@ class User(AbstractUser):
     # is_superuser
     # date_joined
 
-    email = models.EmailField(unique=True,db_index=True,verbose_name="email address")
-    email_verified = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    date_of_birth = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(max_length=20,unique=True,null=True,blank=True)
-    address = models.CharField(max_length=255,null=True,blank=True)
-    profile_image = models.ImageField(upload_to="profileImages/users",null=True,blank=True)
-
     class Meta:
         db_table = "app_users"
         ordering = ["-date_joined"]
 
     def __str__(self):
         return self.email
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
