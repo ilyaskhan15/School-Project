ba590af047160c3d74132b811237ae0dfb5e93c0
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2026-01-11 15:33:54 +0500

    register admin.py of students, teacher

--- Backend/students/admin.py
+++ Backend/students/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Student
+
+
+@admin.register(Student)
+class StudentUserModel(admin.ModelAdmin):
+    list_display = ['user','name','class_level','degree_name','gpa','roll_no' ]
+    search_fields = ['user__first_name', 'user__last_name', 'roll_no']
+    list_display_links = ['user']
+    
+    def name(self, obj):
+        return obj.user.get_full_name()
+    name.short_description = "Student Name"
+    
 
 # Register your models here.
--- Backend/teachers/admin.py
+++ Backend/teachers/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Teacher
+
+@admin.register(Teacher)
+class TeacherModelAdmin(admin.ModelAdmin):
+    list_display = ['user','name','department', 'office_location','hire_date']
+    search_fields = ['department', 'user__first_name', 'user__last_name']
+    
+    
+    def name(self, obj):
+        return obj.user.get_full_name()
+    name.short_description = "Teacher Name"
 
 # Register your models here.
