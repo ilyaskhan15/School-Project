4b7cd069b35034bd5a242e3d7f3f3ae7ca18b24d
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2026-01-13 15:16:09 +0500

    Update the admin.py model

--- Backend/classes/admin.py
+++ Backend/classes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ClassLevel, Section
+
+@admin.register(ClassLevel)
+class ClassLevelModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    
+    
+@admin.register(Section)
+class SectionModelAdmin(admin.ModelAdmin):
+    list_display = ['class_level', 'name']
 
 # Register your models here.
